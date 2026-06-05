4c8ce245909216761533b34097b0545e5f01c1c2
mshikhar516 <mshikhar516@gmail.com>
2026-06-06 01:31:25 +0530

    5th day

---
+++ Q18_StrongNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, original, digit;
+    int sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while(num > 0)
+    {
+        digit = num % 10;
+
+        int fact = 1;
+        for(int i = 1; i <= digit; i++)
+        {
+            fact *= i;
+        }
+
+        sum += fact;
+        num /= 10;
+    }
+
+    if(sum == original)
+        cout << "Strong Number";
+    else
+        cout << "Not a Strong Number";
+
+    return 0;
+}
