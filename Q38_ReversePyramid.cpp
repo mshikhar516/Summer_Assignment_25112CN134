ddc841c45f24bfc7406169b10b1f46c3cb6987ae
mshikhar516 <mshikhar516@gmail.com>
2026-06-10 20:18:09 +0530

    10th day

---
+++ Q38_ReversePyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for(int i = n; i >= 1; i--)
+    {
+        // Spaces
+        for(int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        // Stars
+        for(int j = 1; j <= (2 * i - 1); j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
